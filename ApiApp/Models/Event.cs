26f0d86a6d85cc420689006c24e8e2281f74ef4f
Elliot Berman <elliot@elliotjb.com>
2015-07-27 19:29:52 -0400

    Add base models (team, event, division)

---
+++ ApiApp/Models/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiApp.Models
+{
+    public class Event
+    {
+        public const string REGEX = @"^RE-(VRC|VEXU|VIQC)-[0,1]\d-\d{4}$";
+
+        [Key, Required, MaxLength(17), RegularExpression(REGEX)]
+        public string Sku { get; set; }
+
+        public string Name { get; set; }
+
+        public Program Program { get; set; }
+        public Level Levels { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public string Season { get; set; }
+
+        public struct Venue
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Region { get; set; }
+            public string Country { get; set; }
+        }
+
+        public string Description { get; set; }
+
+        public struct Contact
+        {
+            public string Name { get; set; }
+            public string Title { get; set; }
+            [EmailAddress]
+            public string Email { get; set; }
+            [Phone]
+            public string Phone { get; set; }
+        }
+
+        public string Agenda { get; set; }
+    }
+}
---
+++ ApiApp/Models/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiApp.Models
+{
+    public class Team
+    {
+        public const string REGEX = @"^([1-9]\d{0,3}[A-Z]{0,1}|[A-Z]{1,4}\d{0,1})$";
+
+        [Key, Required, MaxLength(5), RegularExpression(REGEX)]
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+        public string RobotName { get; set; }
+
+        public string Organization { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        public Level Level { get; set; }
+        public Program Program { get; set; }
+    }
+}
